b64cbf578876efa5f19bb4aee35e74e929ea63b5
Xgui4 Studio <134389196+xgui4@users.noreply.github.com>
2025-12-14 02:00:32 -0500

    Add notification functionality and update dependencies in PKGBUILD

--- window.c
+++ window.c
@@ -11,14 +11,25 @@ static void go_to_github() {
     system("xdg-open https://github.com/xgui4/"); 
 }
 
+static void send_notification(GtkCheckButton *checkbox, gpointer user_data) {
+    gboolean is_active = gtk_check_button_get_active(checkbox);
+    if (is_active) {
+        system("notify-send -a hellow-world-gtk -i assets/image.jpeg Hello-World-GTK \"Notfication Enable!\""); 
+    }
+    else {
+        system("notify-send -a hellow-world-gtk -i assets/image.jpeg Hello-World-GTK \"Notfication Disabled!\""); 
+    }
+}
+
 static void activate(GtkApplication *app, gpointer user_data)
 {
     GtkWidget *window;
     GtkWidget *vbox;
-    GtkWidget *button;
-    GtkWidget *image;
+    GtkWidget *hello_world_button;
+    GtkWidget *logo_image;
     GtkWidget *checkbox1; 
-    GtkWidget *button2; 
+    GtkWidget *go_to_github_button;
+    // GtkWidget *text_field; 
 
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), app_name);
@@ -30,32 +41,35 @@ static void activate(GtkApplication *app, gpointer user_data)
     gtk_widget_set_margin_start(vbox, 10);
     gtk_widget_set_margin_end(vbox, 10);
 
-    image = gtk_image_new_from_file("assets/image.jpg");
+    logo_image = gtk_image_new_from_file("assets/image.jpg");
 
-    gtk_widget_set_hexpand(image, TRUE);
-    gtk_widget_set_vexpand(image, TRUE);
+    gtk_widget_set_hexpand(logo_image, TRUE);
+    gtk_widget_set_vexpand(logo_image, TRUE);
 
-    gtk_widget_set_halign(image, GTK_ALIGN_FILL);
-    gtk_widget_set_valign(image, GTK_ALIGN_FILL);
+    gtk_widget_set_halign(logo_image, GTK_ALIGN_FILL);
+    gtk_widget_set_valign(logo_image, GTK_ALIGN_FILL);
 
-    gtk_image_set_pixel_size(GTK_IMAGE(image), 500); 
+    gtk_image_set_pixel_size(GTK_IMAGE(logo_image), 500); 
 
-    button = gtk_button_new_with_label("Hello World From GTK!");
-    gtk_widget_set_halign(button, GTK_ALIGN_CENTER);
-    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+    hello_world_button = gtk_button_new_with_label("Hello World From GTK!");
+    gtk_widget_set_halign(hello_world_button, GTK_ALIGN_CENTER);
+    g_signal_connect(hello_world_button, "clicked", G_CALLBACK(print_hello), NULL);
 
-    checkbox1 = gtk_check_button_new(); 
+    checkbox1 = gtk_check_button_new_with_label("Activate Notification");
 
-    button2 = gtk_button_new_with_label("Go to my Github"); 
+    go_to_github_button = gtk_button_new_with_label("Go to my Github"); 
 
-    g_signal_connect(button2, "clicked", G_CALLBACK(go_to_github), NULL);
+    g_signal_connect(go_to_github_button, "clicked", G_CALLBACK(go_to_github), NULL);
 
+    g_signal_connect(checkbox1, "toggled", G_CALLBACK(send_notification), NULL);
 
-    gtk_box_append(GTK_BOX(vbox), image);
-    gtk_box_append(GTK_BOX(vbox), button);
+    // text_field = gtk_text_new();
+
+    gtk_box_append(GTK_BOX(vbox), logo_image);
+    // gtk_box_append(GTK_BOX(vbox), text_field);
+    gtk_box_append(GTK_BOX(vbox), hello_world_button);
     gtk_box_append(GTK_BOX(vbox), checkbox1); 
-    gtk_box_append(GTK_BOX(vbox), button2); 
+    gtk_box_append(GTK_BOX(vbox), go_to_github_button); 
 
     gtk_window_set_child(GTK_WINDOW(window), vbox);
-    gtk_window_present(GTK_WINDOW(window));
 }
--- window.h
+++ window.h
@@ -4,4 +4,6 @@ static void print_hello(GtkWidget *widget, gpointer  data);
 
 static void go_to_github(); 
 
+static void send_notifcation(GtkCheckButton *checkbox, gpointer user_data); 
+
 static void activate(GtkApplication *app, gpointer user_data);
