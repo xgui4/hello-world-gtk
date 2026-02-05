4644ecec06089b48bbd6ce215e4a6b952c02ed68
xgui4 <134389196+xgui4@users.noreply.github.com>
2026-02-05 16:32:01 -0500

    5 Februrary : Removed useless header, added vscode support for clang and clangd and codelldb debugging support with meson

--- src/utils.c
+++ src/utils.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "utils.h"
 #include <gtk/gtk.h>
 #include <string.h>
--- src/widgets/button.c
+++ src/widgets/button.c
@@ -1,10 +1,5 @@
 #include <gtk/gtk.h>
-#include <stdlib.h>
-#include "../window.h"
-#include "../config.h"
-#include "../app_data.h"
 #include "button.h"
-#include "../myresources.h"
 
 GtkWidget* button_init(const char* default_label, GtkAlign align, GCallback callback, gpointer observed_widget) {
     GtkWidget *button = gtk_button_new_with_label(default_label);
